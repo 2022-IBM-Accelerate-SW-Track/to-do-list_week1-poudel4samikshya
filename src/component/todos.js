5628bd6c661c7ddf03467898aa8ff1ff88b66b3e
Samikshya_poudel <50699869+poudel4samikshya@users.noreply.github.com>
2022-06-08 19:57:54 -0400

    changed the component

--- src/component/todos.js
+++ src/component/todos.js
@@ -17,7 +17,8 @@ const Todos = ({ todos }) => {
               {/* Remember, we set the local state of this todo item when the user submits the form in 
               AddTodo.js. All we need to do is return the todo list item */}
               <CardContent>
-                <span style={{ padding: "50px" }}>add todo content</span>
+                <span style={{ padding: "50px" }}>Go to department store</span>
+                <span style={{ padding: "50px" }}>Buy groceries</span>
               </CardContent>
             </Card>
           </Grid>
@@ -25,7 +26,7 @@ const Todos = ({ todos }) => {
       })
     ) : (
       <p>{/*Enter your message here if todo list is empty*/}</p>
-    );
+    );s
     // Lastly, return the todoList constant that we created above to show all of the items on the screen.
     return (
       <div className="todoCollection" style={{ padding: "10px" }}>
